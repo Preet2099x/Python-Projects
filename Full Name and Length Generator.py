54dff292883d7160f5dd09401b2adb279765c8da
Preet <preet2099x@gmail.com>
2023-11-23 02:12:09 +0530

    Added Length calculating function

--- Full Name and Length Generator.py
+++ Full Name and Length Generator.py
@@ -4,4 +4,3 @@ last_name=input("What is the your last name?\n")
 full_name=first_name+" "+last_name
 print("Your Name is "+full_name.title()+ " and its "+str(len(full_name)-1)+ " characters long.")
 
-
