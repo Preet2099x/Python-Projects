4889fe332f11815375a03ad6c91cdb159ffa1a6c
Preet <preet2099x@gmail.com>
2023-11-22 23:21:54 +0530

    made a python program to generatre Full Name and Calculate it's length

---
+++ Full Name and Length Generator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Full Name Generator!")
+first_name=input("What is the your first name?\n")
+last_name=input("What is the your last name?\n")
+full_name=first_name+" "+last_name
+print("Your Name is "+full_name.title()+ " and its "+str(len(full_name)-1)+ " characters long.")
+
+
