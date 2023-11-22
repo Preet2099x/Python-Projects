a6b9931c3cca22e15515e79165b1e0ecd1e7ff30
Preet <preet2099x@gmail.com>
2023-11-23 02:02:15 +0530

    added Length Generator to the opening line

--- Full Name and Length Generator.py
+++ Full Name and Length Generator.py
@@ -1,4 +1,4 @@
-print("Welcome to the Full Name Generator!")
+print("Welcome to the Full Name and Length Generator!")
 first_name=input("What is the your first name?\n")
 last_name=input("What is the your last name?\n")
 full_name=first_name+" "+last_name
