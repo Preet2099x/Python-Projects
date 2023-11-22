6028657ef9f59ffd0547bf1fbd7503f37b47e29f
Preet Singh <150048406+Preet2099x@users.noreply.github.com>
2023-11-22 23:28:53 +0530

    Delete Full Name Generator.py

--- Full Name Generator.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the Full Name Generator!")
-first_name=input("What is the your first name?\n")
-last_name=input("What is the your last name?\n")
-full_name=first_name+" "+last_name
-print("Your Name is "+full_name.title())
-
